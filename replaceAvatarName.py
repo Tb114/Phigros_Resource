bf0355e26b15adaa178cdb4f635771b8a44741d1
Tb114 <tb1145@outlook.com>
2025-11-23 00:23:08 +0800

    111

--- replaceAvatarName.py
+++ replaceAvatarName.py
@@ -11,16 +11,19 @@ with open('./info/tmp.tsv','r', encoding='utf-8') as f:
         except:
             continue
         mp[each.split('\t')[1]] = each.split('\t')[0]
-
+# print(mp)
 for key, value in mp.items():
     old_file = os.path.join('avatar/', f"{key}.png")
     new_file = os.path.join('avatar/', f"{value}.png")
     # 检查原文件是否存在
     if os.path.exists(old_file):
-        if key == 'Cipher1':
+        if value == 'Cipher1':
             continue
         try:
             os.rename(old_file, new_file)
+            print(f'Rename {old_file} into {new_file}')
         except:
             pass
+    else:
+        print(old_file)
         
